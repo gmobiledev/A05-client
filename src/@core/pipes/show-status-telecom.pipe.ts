3189329828b35356e8fd360251fca0463e433de8
anhtuan3003 <nevermorethe@gmail.com>
2025-07-10 09:45:04 +0700

    fix

--- src/@core/pipes/show-status-telecom.pipe.ts
+++ src/@core/pipes/show-status-telecom.pipe.ts
@@ -20,14 +20,6 @@ export class ShowStatusTelecomPipe implements PipeTransform {
         html = '<span class="badge badge-pill badge-light-danger mr-1">Đã hủy</span>'
     } else if (value === TaskTelecomStatus.STATUS_INIT) {
         html = '<span class="badge badge-pill badge-light-warning mr-1">Đại lý chưa hoàn thiện thông tin</span>'
-    } else if (value === TaskTelecomStatus.STATUS_SUCCESS_PART) {
-        html = '<span class="badge badge-pill badge-light-success mr-1">Thành công 1 phần</span>'
-    }
-    else if (value === TaskTelecomStatus.STATUS_Waiting_For_Payment) {
-        html = '<span class="badge badge-pill badge-light-warning mr-1">Chờ thanh toán</span>'
-    }
-    else if (value === TaskTelecomStatus.STATUS_Waiting_For_Information) {
-        html = '<span class="badge badge-pill badge-light-warning mr-1">Chờ thông tin</span>'
     }
     return html;
   }
